4d4fd5f3dc25e0545c28049bfb75285d87753456
Juampa1998 <Juampa_junin@hotmail.com>
2024-09-01 21:31:35 -0300

    Ruta de Registro finish

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,16 +1,21 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+import userRoutes from "./routes/users";
 
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/api/test", async (req: Request, res: Response) => {
-    res.json({message: 'Holaaa estamos corriendo nuestro puerto.'})
+  res.json({ message: "Holaaa estamos corriendo nuestro puerto." });
 });
 
+app.use("/api/users", userRoutes);
+
 app.listen(7000, () => {
-    console.log("Server corriendo en el puerto: 7000")
-})
+  console.log("Server corriendo en el puerto: 7000");
+});
